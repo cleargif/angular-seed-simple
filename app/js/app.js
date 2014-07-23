87fff6cbd6358a6de5baf6cc016ddcff742b17f9
David Plews (@cleargif) <freelance@cleargif.co.uk>
2014-07-23 11:49:43 +0100

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'ngRoute',
+  'ui.bootstrap',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
+]).
+config(['$routeProvider',
+  function ($routeProvider) {
+    $routeProvider.when('/home', {
+      templateUrl: 'views/partials/home.html',
+      controller: 'CtrlHome'
+    });
+    $routeProvider.when('/about', {
+      templateUrl: 'views/partials/about.html',
+      controller: 'CtrlAbout'
+    });
+    $routeProvider.when('/contact', {
+      templateUrl: 'views/partials/contact.html',
+      controller: 'CtrlContact'
+    });
+    $routeProvider.when('/404', {
+      templateUrl: 'views/partials/404.html',
+      controller: 'Ctrl404'
+    });
+    $routeProvider.when('/', {
+      redirectTo: '/home'
+    });
+    $routeProvider.otherwise({
+      redirectTo: '/404'
+    });
+  }
+]);
